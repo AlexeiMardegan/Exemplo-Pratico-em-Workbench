91ac1ed0b9694575745da5ba9a0d0567faa290d9
Alexei Mardegan <76541124+AlexeiMardegan@users.noreply.github.com>
2022-08-31 17:12:41 -0300

    Update loja cliente e ator.sql
    adicionado pagamentos feito por cliente 12 que seja acima de 5
    e pagamentos que funcionario 1 efeituou

--- loja cliente e ator.sql
+++ loja cliente e ator.sql
@@ -32,4 +32,8 @@ where filme.filme_id=filme_ator.filme_id and
 filme_ator.ator_id=ator.ator_id
 and ator.ator_id in (33,19);
 
-select * from filme_ator;
+select * from pagamento;
+select * from pagamento where valor>5.00 and cliente_id=12;
+
+select * from pagamento where funcionario_id=1;
+select * from pagamento where  funcionario_id>1;
